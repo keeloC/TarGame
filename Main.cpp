d386599b0aae257470df3cba5baa92c1bfadaa6f
Keelo_Code <suaresthomas20@gmail.com>
2026-05-02 17:39:03 -0500

    commit

--- Main.cpp
+++ Main.cpp
@@ -1,31 +1,35 @@
-#include "SFML/Graphics/Color.hpp"
+#include "SFML/System/Clock.hpp"
 #include <SFML/Graphics.hpp>
+#include <algorithm>
 
 int main() {
   sf::RenderWindow window(sf::VideoMode(800, 600), "My Window");
   sf::View view(sf::FloatRect(0, 0, 800, 600));
 
-  float playerSize = 40.f;
+  float playerSize = 10.f;
   float rectWidth = 32.f;
   float rectHeight = 35.f;
 
   int FilaP = 14;
+  int FilaPiedra = 28;
 
   float playerSpeed = 0.3f;
+
   float playerX = 800.f / 2.f - playerSize / 2.f;
   float playerY = FilaP * rectHeight - playerSize;
 
-  // Gravedad
-
+  // Física
   float gravedad = 0.004f;
   float velocidadY = 0.f;
-
-  // salto
-
   float velocidadSalto = 1.f;
   float VelMaxDown = 0.5f;
 
-  // bool
+  // Time of mining
+
+  sf::Clock MinaT;
+  float tiempodeminado = 1.f;
+  int minaCol = -1;
+  int minaRow = -1;
 
   bool enElPiso = true;
   sf::Clock saltoClock;
@@ -36,82 +40,173 @@ int main() {
   const int COLS = 200;
   int mundo[FILAS][COLS];
 
+  // Generación mundo
   for (int i = 0; i < FILAS; ++i) {
     for (int j = 0; j < COLS; ++j) {
-      if (i >= FilaP) {
+      if (i >= FilaPiedra)
+        mundo[i][j] = 2;
+      else if (i >= FilaP)
         mundo[i][j] = 1;
-      } else {
+      else
         mundo[i][j] = 0;
-      }
     }
   }
 
+  saltoClock.restart();
+
   while (window.isOpen()) {
     sf::Event event;
-
     while (window.pollEvent(event)) {
       if (event.type == sf::Event::Closed)
         window.close();
     }
 
+    // Minería
+    sf::Vector2i pixelPos = sf::Mouse::getPosition(window);
+    sf::Vector2f worldPos = window.mapPixelToCoords(pixelPos);
+
+    int bloqueCol = static_cast<int>(worldPos.x / rectWidth);
+    int bloqueRow = static_cast<int>(worldPos.y / rectHeight);
+
+    if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+      if (bloqueCol >= 0 && bloqueCol < COLS && bloqueRow >= 0 &&
+          bloqueRow < FILAS) {
+
+        if (bloqueCol != minaCol || bloqueRow != minaRow) {
+
+          MinaT.restart();
+          minaCol = bloqueCol;
+          minaRow = bloqueRow;
+        }
+
+        if (MinaT.getElapsedTime().asSeconds() >= tiempodeminado) {
+          mundo[bloqueRow][bloqueCol] = 0;
+          minaCol = -1;
+          minaRow = -1;
+        }
+      }
+    }
+
+    // Movimiento horizontal
     float velocidadActual = playerSpeed;
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
-      velocidadActual = playerSpeed * 1.8f;
+      velocidadActual *= 1.8f;
+
+    float newPosX = playerPos.x;
 
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-      playerPos.x -= velocidadActual;
+      newPosX -= velocidadActual;
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-      playerPos.x += velocidadActual;
+      newPosX += velocidadActual;
 
+    // Colisión horizontal
+    int filaArriba = static_cast<int>((playerPos.y + 1) / rectHeight);
+    int filaAbajo =
+        static_cast<int>((playerPos.y + playerSize - 1) / rectHeight);
+
+    int colIzq = static_cast<int>(newPosX / rectWidth);
+    int colDer = static_cast<int>((newPosX + playerSize) / rectWidth);
+
+    bool colisionX = false;
+
+    for (int i = filaArriba; i <= filaAbajo; ++i) {
+      if (i >= 0 && i < FILAS) {
+        if ((colIzq >= 0 && colIzq < COLS && mundo[i][colIzq] > 0) ||
+            (colDer >= 0 && colDer < COLS && mundo[i][colDer] > 0)) {
+          colisionX = true;
+          break;
+        }
+      }
+    }
+
+    if (!colisionX)
+      playerPos.x = newPosX;
+
+    // Física vertical
     enElPiso = false;
 
     velocidadY += gravedad;
-    playerPos.y += velocidadY;
-
-    if (velocidadY > VelMaxDown) {
+    if (velocidadY > VelMaxDown)
       velocidadY = VelMaxDown;
+
+    float newPosY = playerPos.y + velocidadY;
+
+    int bottomRow = static_cast<int>((newPosY + playerSize) / rectHeight);
+    int topRow = static_cast<int>(newPosY / rectHeight);
+
+    colIzq = static_cast<int>(playerPos.x / rectWidth);
+    colDer = static_cast<int>((playerPos.x + playerSize) / rectWidth);
+
+    // Colisión vertical (caída)
+    if (velocidadY > 0) {
+      if (bottomRow >= 0 && bottomRow < FILAS) {
+        if ((colIzq >= 0 && colIzq < COLS && mundo[bottomRow][colIzq] > 0) ||
+            (colDer >= 0 && colDer < COLS && mundo[bottomRow][colDer] > 0)) {
+
+          playerPos.y = bottomRow * rectHeight - playerSize;
+          velocidadY = 0;
+          enElPiso = true;
+        } else {
+          playerPos.y = newPosY;
+        }
+      }
+    }
+    // Colisión vertical (subida)
+    else if (velocidadY < 0) {
+      if (topRow >= 0 && topRow < FILAS) {
+        if ((colIzq >= 0 && colIzq < COLS && mundo[topRow][colIzq] > 0) ||
+            (colDer >= 0 && colDer < COLS && mundo[topRow][colDer] > 0)) {
+
+          playerPos.y = (topRow + 1) * rectHeight;
+          velocidadY = 0;
+        } else {
+          playerPos.y = newPosY;
+        }
+      }
     }
 
-    if (playerPos.y + playerSize >= FilaP * rectHeight) {
-      playerPos.y = FilaP * rectHeight - playerSize;
-      velocidadY = 0;
-      enElPiso = true;
-    }
-
+    // Salto
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && enElPiso &&
-        saltoClock.getElapsedTime().asSeconds() >= 0.5f) {
+        saltoClock.getElapsedTime().asSeconds() >= 0.2f) {
+
       velocidadY = -velocidadSalto;
       saltoClock.restart();
     }
 
+    // Jugador
     sf::RectangleShape player(sf::Vector2f(playerSize, playerSize));
     player.setPosition(playerPos);
     player.setFillColor(sf::Color::Red);
 
+    // Cámara
     view.setCenter(playerPos.x + playerSize / 2.f,
                    playerPos.y + playerSize / 2.f);
 
     window.clear(sf::Color::Black);
-
     window.setView(view);
 
     int colInicio = std::max(0, (int)((view.getCenter().x - 400) / rectWidth));
     int colFin =
         std::min(COLS, (int)((view.getCenter().x + 400) / rectWidth) + 1);
+
     int filaInicio =
         std::max(0, (int)((view.getCenter().y - 300) / rectHeight));
     int filaFin =
-        std::min(FILAS - 1, (int)((view.getCenter().y + 300) / rectHeight) + 1);
+        std::min(FILAS, (int)((view.getCenter().y + 300) / rectHeight) + 1);
 
     for (int i = filaInicio; i < filaFin; ++i) {
       for (int j = colInicio; j < colFin; ++j) {
-        sf::RectangleShape rectangle(sf::Vector2f(rectWidth, rectHeight));
+        sf::RectangleShape rect(sf::Vector2f(rectWidth, rectHeight));
 
         if (mundo[i][j] == 1) {
-          rectangle.setFillColor(sf::Color::Blue);
-          rectangle.setPosition(j * rectWidth, i * rectHeight);
-          window.draw(rectangle);
-        }
+          rect.setFillColor(sf::Color::Blue);
+        } else if (mundo[i][j] == 2) {
+          rect.setFillColor(sf::Color::Magenta);
+        } else
+          continue;
+
+        rect.setPosition(j * rectWidth, i * rectHeight);
+        window.draw(rect);
       }
     }
 
